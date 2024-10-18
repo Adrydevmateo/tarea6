d134d3dd01ad778409c3849751c46ac6f3250323
AdryDev <adrydevmateo@gmail.com>
2024-10-17 20:38:14 -0400

    Initial Commit

--- src/components/Menu.tsx
+++ src/components/Menu.tsx
@@ -1,100 +1,94 @@
 import {
-  IonContent,
-  IonIcon,
-  IonItem,
-  IonLabel,
-  IonList,
-  IonListHeader,
-  IonMenu,
-  IonMenuToggle,
-  IonNote,
+    IonContent,
+    IonIcon,
+    IonItem,
+    IonLabel,
+    IonList,
+    IonListHeader,
+    IonMenu,
+    IonMenuToggle,
+    IonNote,
 } from '@ionic/react';
 
 import { useLocation } from 'react-router-dom';
-import { archiveOutline, archiveSharp, bookmarkOutline, heartOutline, heartSharp, mailOutline, mailSharp, paperPlaneOutline, paperPlaneSharp, trashOutline, trashSharp, warningOutline, warningSharp } from 'ionicons/icons';
+import { archiveOutline, archiveSharp, heartOutline, heartSharp, mailOutline, mailSharp, paperPlaneOutline, paperPlaneSharp, trashOutline, trashSharp, warningOutline, warningSharp } from 'ionicons/icons';
 import './Menu.css';
 
 interface AppPage {
-  url: string;
-  iosIcon: string;
-  mdIcon: string;
-  title: string;
+    url: string;
+    iosIcon: string;
+    mdIcon: string;
+    title: string;
 }
 
 const appPages: AppPage[] = [
-  {
-    title: 'Inbox',
-    url: '/folder/Inbox',
-    iosIcon: mailOutline,
-    mdIcon: mailSharp
-  },
-  {
-    title: 'Outbox',
-    url: '/folder/Outbox',
-    iosIcon: paperPlaneOutline,
-    mdIcon: paperPlaneSharp
-  },
-  {
-    title: 'Favorites',
-    url: '/folder/Favorites',
-    iosIcon: heartOutline,
-    mdIcon: heartSharp
-  },
-  {
-    title: 'Archived',
-    url: '/folder/Archived',
-    iosIcon: archiveOutline,
-    mdIcon: archiveSharp
-  },
-  {
-    title: 'Trash',
-    url: '/folder/Trash',
-    iosIcon: trashOutline,
-    mdIcon: trashSharp
-  },
-  {
-    title: 'Spam',
-    url: '/folder/Spam',
-    iosIcon: warningOutline,
-    mdIcon: warningSharp
-  }
+    {
+        title: 'Inicio',
+        url: '/folder/Inicio',
+        iosIcon: mailOutline,
+        mdIcon: mailSharp
+    },
+    {
+        title: 'Genero',
+        url: '/folder/Genero',
+        iosIcon: paperPlaneOutline,
+        mdIcon: paperPlaneSharp
+    },
+    {
+        title: 'Edad',
+        url: '/folder/Edad',
+        iosIcon: heartOutline,
+        mdIcon: heartSharp
+    },
+    {
+        title: 'Pais',
+        url: '/folder/Pais',
+        iosIcon: archiveOutline,
+        mdIcon: archiveSharp
+    },
+    {
+        title: 'Clima',
+        url: '/folder/Clima',
+        iosIcon: trashOutline,
+        mdIcon: trashSharp
+    },
+    {
+        title: 'Noticias',
+        url: '/folder/Noticias',
+        iosIcon: warningOutline,
+        mdIcon: warningSharp
+    },
+    {
+        title: 'Acerca De',
+        url: '/folder/Acerca-de',
+        iosIcon: warningOutline,
+        mdIcon: warningSharp
+    }
 ];
 
-const labels = ['Family', 'Friends', 'Notes', 'Work', 'Travel', 'Reminders'];
-
 const Menu: React.FC = () => {
-  const location = useLocation();
+    const location = useLocation();
 
-  return (
-    <IonMenu contentId="main" type="overlay">
-      <IonContent>
-        <IonList id="inbox-list">
-          <IonListHeader>Inbox</IonListHeader>
-          <IonNote>hi@ionicframework.com</IonNote>
-          {appPages.map((appPage, index) => {
-            return (
-              <IonMenuToggle key={index} autoHide={false}>
-                <IonItem className={location.pathname === appPage.url ? 'selected' : ''} routerLink={appPage.url} routerDirection="none" lines="none" detail={false}>
-                  <IonIcon aria-hidden="true" slot="start" ios={appPage.iosIcon} md={appPage.mdIcon} />
-                  <IonLabel>{appPage.title}</IonLabel>
-                </IonItem>
-              </IonMenuToggle>
-            );
-          })}
-        </IonList>
-
-        <IonList id="labels-list">
-          <IonListHeader>Labels</IonListHeader>
-          {labels.map((label, index) => (
-            <IonItem lines="none" key={index}>
-              <IonIcon aria-hidden="true" slot="start" icon={bookmarkOutline} />
-              <IonLabel>{label}</IonLabel>
-            </IonItem>
-          ))}
-        </IonList>
-      </IonContent>
-    </IonMenu>
-  );
+    return (
+        <IonMenu contentId="main" type="overlay">
+            <IonContent>
+                <IonList id="inbox-list">
+                    <IonListHeader>AdryDev</IonListHeader>
+                    <IonNote>adrydevmateo@gmail.com</IonNote>
+                    {appPages.map((appPage, index) => {
+                        return (
+                            <IonMenuToggle key={index} autoHide={false}>
+                                <IonItem className={location.pathname === appPage.url ? 'selected' : ''} routerLink={appPage.url} routerDirection="none" lines="none" detail={false}>
+                                    <IonIcon aria-hidden="true" slot="start" ios={appPage.iosIcon} md={appPage.mdIcon} />
+                                    <IonLabel>{appPage.title}</IonLabel>
+                                </IonItem>
+                            </IonMenuToggle>
+                        );
+                    })}
+                </IonList>
+            </IonContent>
+        </IonMenu>
+    );
 };
 
 export default Menu;
