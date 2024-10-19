a2afe2ddc6ac7518a9f368917e9d920ae8dd0a1a
AdryDev <adrydevmateo@gmail.com>
2024-10-18 20:11:01 -0400

    Inicio page added

--- src/pages/Page.tsx
+++ src/pages/Page.tsx
@@ -3,6 +3,7 @@ import { useParams } from 'react-router';
 import ExploreContainer from '../components/ExploreContainer';
 import './Page.css';
 import Genero from './Genero';
+import Inicio from './Inicio';
 
 const Page: React.FC = () => {
 
@@ -25,6 +26,9 @@ const Page: React.FC = () => {
                         <IonTitle size="large">{name}</IonTitle>
                     </IonToolbar>
                 </IonHeader>
+                <ExploreContainer>
+                    {name == "Inicio" ? <Inicio /> : null}
+                </ExploreContainer>
                 <ExploreContainer>
                     {name == "Genero" ? <Genero /> : null}
                 </ExploreContainer>
